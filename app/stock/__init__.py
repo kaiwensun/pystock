beca575c92178e841c54c88f87e39f0deb95ecbf
Kaiwen Sun <skw_kevin@126.com>
2018-12-03 02:17:05 -0800

    monitor stocks

--- app/stock/__init__.py
+++ app/stock/__init__.py
@@ -1,10 +1,61 @@
 import robin_stocks
+import datetime
+import pytz
+import time
+import os
 
 from config import settings
 from app.stock import infomation
 
 login = robin_stocks.login(settings.USER_EMAIL, settings.USER_PASSWORD)
 
+_LAST_FILE_HOUR_ = None
+_LOG_FILE_NAME_ = None
+
 
 def run_service():
-    infomation.build_holdings()
+    while True:
+        holdings = None
+        market_info = infomation.market_open_time(settings.MARKETS[0])
+        if not market_info['is_open_today']:
+            logger("sleep during holiday")
+            time.sleep(60 * 60)
+        elif market_info['is_open_now']:
+            logger("work during open hours")
+            holdings = infomation.build_holdings()
+        elif market_info['end - utcnow'] <= 0:
+            logger("sleep after market closed")
+            time.sleep(60 * 60)
+        elif market_info['utcnow - start'] > -60 * 5:
+            logger("work when market is about to open")
+            holdings = infomation.build_holdings()
+        else:
+            logger("sleep when market has not opened yet")
+            time.sleep(- market_info['utcnow - start'] - 60 * 3)
+        if holdings is not None:
+            logger(holdings)
+        time.sleep(settings.OPEN_HOUR_SLEEP)
+
+
+def logger(info):
+    global _LAST_FILE_HOUR_
+    global _LOG_FILE_NAME_
+    import pprint
+    pprint.pprint(info)
+    timestamp = datetime.datetime.now(tz=pytz.timezone(settings.TIMEZONE))
+    hour = timestamp.hour
+    if _LAST_FILE_HOUR_ != hour:
+        _LAST_FILE_HOUR_ = hour
+        time_format = '%Y-%m-%dT%H%M%Sz%Z'
+        timestamp.strftime(time_format)
+        _LOG_FILE_NAME_ = os.path.join(
+            "logs", "{}.log".format(
+                timestamp.strftime(time_format)))
+        if not os.path.exists(os.path.dirname(_LOG_FILE_NAME_)):
+            os.makedirs(os.path.dirname(_LOG_FILE_NAME_))
+    logtext = pprint.pformat(info, indent=4)
+    try:
+        with open(_LOG_FILE_NAME_, "a") as myfile:
+            myfile.write(logtext + '\n')
+    finally:
+        myfile.close()
--- app/stock/infomation.py
+++ app/stock/infomation.py
@@ -1,7 +1,144 @@
+import datetime
+import pytz
 import robin_stocks
+
+from config import settings
+
+_NEED_UPDATE = set()
+_MONITORED_STOCKS = {}
+_ACCOUNT_NUMBER = None
+
+
+def mark_need_update(symbol):
+    _NEED_UPDATE.add(symbol)
+
+
+def _extract_obj(obj_or_lst, keys, rename={}):
+    """
+    Extract from dict(s) to get only key-value pairs that we are interested in.
+
+    :param obj_or_lst: The dict that we want to extract from, or a list of \
+    dicts.
+    :param keys: The list of keys that we are interested in.
+    :param renames: a mapping to rename the keys of the output dict.
+    :return: the extracted dict, or a list of extracted dicts.
+
+    """
+    if isinstance(obj_or_lst, dict):
+        is_dict = True
+        lst = [obj_or_lst]
+    else:
+        is_dict = False
+        lst = obj_or_lst
+    extracted_lst = []
+    for obj in lst:
+        extracted = {}
+        for key in keys:
+            extracted[rename.get(key, key)] = obj[key]
+        extracted_lst.append(extracted)
+    return extracted_lst[0] if is_dict else extracted_lst
+
+
+def get_fundamentals(symbols):
+    fundamental_data = robin_stocks.stocks.get_fundamentals(symbols)
+    keys = ['open', 'high', 'low']
+    return _extract_obj(fundamental_data, keys)
+
+
+def get_instruments(symbols):
+    instrument_data = robin_stocks.stocks.get_instruments_by_symbols(symbols)
+    keys = ['tradability', 'rhs_tradability', "simple_name", "id"]
+    renames = {"id": "stock_id"}
+    return _extract_obj(instrument_data, keys, renames)
+
+
+def get_quotes(symbols):
+    quote_data = robin_stocks.stocks.get_quotes(symbols)
+    keys = ["ask_price", "ask_size", "bid_price", "bid_size",
+            "last_trade_price", "last_extended_hours_trade_price"]
+    extracted_data = _extract_obj(quote_data, keys)
+    for item in extracted_data:
+        item['latest_price'] = (
+            item['last_extended_hours_trade_price'] or
+            item['last_trade_price'])
+    return extracted_data
+
+
+def get_positions(stock_ids):
+    account_number = get_account_number()
+    url = "https://api.robinhood.com/accounts/{account_number}/positions/{{stock_id}}/".format(  # noqa
+        account_number=account_number)
+    positions = []
+    for stock_id in stock_ids:
+        position = robin_stocks.helper.request_get(
+            url.format(stock_id=stock_id))
+        positions.append(position)
+    keys = ["shares_held_for_buys", "average_buy_price",
+            "shares_held_for_sells", "quantity"]
+    return _extract_obj(positions, keys)
+
+
+def get_account_number():
+    global _ACCOUNT_NUMBER
+    if _ACCOUNT_NUMBER is None:
+        _ACCOUNT_NUMBER = robin_stocks.profiles \
+            .load_account_profile()['account_number']
+    return _ACCOUNT_NUMBER
+
+
+def get_timestamp():
+    time_format = '%m/%d/%Y %H:%M:%S %Z'
+    timestamp = datetime.datetime.now(tz=pytz.timezone(settings.TIMEZONE))
+    return timestamp.strftime(time_format)
+
+
+def market_open_time(mic):
+    utcnow = datetime.datetime.now(tz=pytz.utc)
+    res = {
+        'is_open_today': False,
+        'is_open_now': False,
+        'extended_opens_at': None,
+        'extended_closes_at': None,
+        'utcnow': utcnow,
+        'utcnow - open': None,
+        'end - utcnow': None
+    }
+    url = "https://api.robinhood.com/markets/{mic}/".format(mic=mic)
+    market = robin_stocks.helper.request_get(url)
+    today_url = market['todays_hours']
+    today_market = robin_stocks.helper.request_get(today_url)
+    is_open = today_market['is_open']
+    if not is_open:
+        return res
+    start = _parse_market_time(today_market['extended_opens_at'])
+    end = _parse_market_time(today_market['extended_closes_at'])
+    res['extended_opens_at'] = start
+    res['extended_closes_at'] = end
+    res['utcnow - open'] = (utcnow - start).total_seconds()
+    res['end - utcnow'] = (end - utcnow).total_seconds()
+    if res['utcnow - open'] > 0 and res['end - utcnow'] > 0:
+        res['is_open_now'] = True
+    return res
+
+
+def _parse_market_time(str):
+    # example: "2018-12-03T14:00:00Z"
+    return datetime.datetime.strptime(
+        str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.UTC)
 
 
 def build_holdings():
-    holdings = robin_stocks.build_holdings()
-    print(holdings)
+    symbols = settings.ALLOWED_SYMBOLS.copy()
+    fundamentals = get_fundamentals(symbols)
+    instruments = get_instruments(symbols)
+    quotes = get_quotes(symbols)
+    stock_ids = [instrument['stock_id'] for instrument in instruments]
+    positions = get_positions(stock_ids)
+    timestamp = get_timestamp()
+    holdings = [{'timestamp': timestamp} for _ in symbols]
+    for i in range(len(holdings)):
+        holdings[i]['symbol'] = symbols[i]
+        datetime.datetime.now(tz=pytz.utc)
+        for data_lst in [fundamentals, instruments, quotes, positions]:
+            holdings[i].update(data_lst[i])
     return holdings
